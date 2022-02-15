9b1bdd2e681042441d64a0d8411c049d7abc225d
Flavien Schriever <flavien@schriever.dev>
2022-02-15 09:45:26 +0100

    fix script

--- gen_input.sh
+++ gen_input.sh
@@ -1,9 +1,6 @@
-#!/bin/sh -e
+#!/bin/bash -e
 
 DIR="$1"
 NAME="$2"
 
-TOTAL=`find "$DIR" -name "$NAME" | wc -l`
-NUMBERS=`find "$DIR" -name "$NAME" -exec wc -l {} \; | awk '{ print $1 }'`
-
-printf "%s\n%s\n" $TOTAL $NUMBERS
+cat <(find "$DIR" -name "$NAME" | wc -l) <(find "$DIR" -name "$NAME" -exec wc -l {} \; | awk '{ print $1 }')
